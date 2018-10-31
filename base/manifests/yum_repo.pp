1a059cdfae779ef30ca09058b0253ade13e44700
cloud-op <jiaozhenqing@gmail.com>
2018-10-31 18:11:50 +0800

    Update yum_repo.pp

--- base/manifests/yum_repo.pp
+++ base/manifests/yum_repo.pp
@@ -1,26 +1,26 @@
 #说明：统一配置yum源并清理yum缓存
 class base::yum_repo {
 	file{
-       		"/etc/yum.repos.d/epel.repo":
-        	owner  => "root",
-        	group  => "root",
-        	mode   => "0644",
-        	source => "puppet:///modules/base/epel.repo",
-		notify => "Exec[yum clean all]";
+               "/etc/yum.repos.d/epel.repo":
+               owner  => "root",
+               group  => "root",
+               mode   => "0644",
+               source => "puppet:///modules/base/epel.repo",
+               notify => "Exec[yum clean all]";
         
-	        "/etc/yum.repos.d/centos.repo":
-                owner  => "root",
-                group  => "root",
-                mode   => "0644",
-                source => "puppet:///modules/base/centos.repo",
-		notify => "Exec[yum clean all]";
+               "/etc/yum.repos.d/centos.repo":
+               owner  => "root",
+               group  => "root",
+               mode   => "0644",
+               source => "puppet:///modules/base/centos.repo",
+               notify => "Exec[yum clean all]";
 	}
 	exec {"yum clean all":
-		user        => "root",
-		group       => "root",
-		path        => "/usr/bin:/usr/sbin:/sbin:/bin",
-		provider    => "shell",
-		timeout     => "180",
-		refreshonly => "true";
+               user        => "root",
+               group       => "root",
+               path        => "/usr/bin:/usr/sbin:/sbin:/bin",
+               provider    => "shell",
+               timeout     => "180",
+               refreshonly => "true";
 	}
 }
