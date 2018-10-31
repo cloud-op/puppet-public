99ef251c7450aa2e0497181b6e5f56f0cd3d49c4
cloud-op <jiaozhenqing@gmail.com>
2018-10-31 18:22:43 +0800

    Update iptables.pp

--- base/manifests/iptables.pp
+++ base/manifests/iptables.pp
@@ -1,4 +1,9 @@
-#清空iptables列表
+#策略用途：清空iptables列表
+#策略说明：仅清空filter表，不会清空mangle等其他表
+#策略说明：在集群运维中，轻易不会使用Iptables进行策略防护，这是可以清空的前提；因此一旦有错误策略设置，那么puppet可以将其清空
+#策略风险：有；如果当前所有/部分机器依赖于iptables进行一些规则过滤，那么该策略会导致这些策略失效
+#策略验证：执行完毕后，执行iptables -nL命令，可以检查有无残存策略
+
 class base::iptables {
         exec { "iptables -F":
                 user     => "root",
@@ -7,4 +12,3 @@ class base::iptables {
                 timeout  => "60",
         }
 }
-
