ca2d8990bbb8aa722892eb75573f64a8e7c88e37
cloud-op <jiaozhenqing@gmail.com>
2018-11-08 13:07:29 +0800

    Update package.pp
    安装nagios-plugins-all

--- base/manifests/package.pp
+++ base/manifests/package.pp
@@ -7,7 +7,7 @@
 
 class base::package {
 	package {
-		["bind-utils","fping","cronolog","bash-completion","iftop","vim","sysstat","traceroute","lsof","strace","blktrace","perf","dstat","tcpdump","rsync","procps","psmisc","rsyslog","inotify-tools","readline-devel","snappy","lzo","tree","gcc","epel-release","bc","yum-plugin-fastestmirror","iotop","nethogs"]:
+		["bind-utils","fping","cronolog","bash-completion","iftop","vim","sysstat","traceroute","lsof","strace","blktrace","perf","dstat","tcpdump","rsync","procps","psmisc","rsyslog","inotify-tools","readline-devel","snappy","lzo","tree","gcc","epel-release","bc","yum-plugin-fastestmirror","iotop","nagios-plugins-all","nethogs"]:
 		ensure  => "installed",
         }
 }
