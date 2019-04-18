c79740b089a8802a215125dfc11c433f2b323797
cloud-op <jiaozhenqing@gmail.com>
2019-04-18 14:47:20 +0800

    add ansible

--- base/manifests/package.pp
+++ base/manifests/package.pp
@@ -7,7 +7,7 @@
 
 class base::package {
 	package {
-		["bind-utils","fping","cronolog","bash-completion","iftop","vim","sysstat","traceroute","lsof","strace","blktrace","perf","dstat","tcpdump","rsync","procps","psmisc","rsyslog","inotify-tools","readline-devel","snappy","lzo","tree","gcc","epel-release","bc","yum-plugin-fastestmirror","iotop","nagios-plugins-all","golang","nethogs"]:
+		["bind-utils","fping","cronolog","bash-completion","iftop","vim","sysstat","traceroute","lsof","strace","blktrace","perf","dstat","tcpdump","rsync","procps","psmisc","rsyslog","inotify-tools","readline-devel","snappy","lzo","tree","gcc","epel-release","bc","yum-plugin-fastestmirror","iotop","nagios-plugins-all","golang","nethogs","ansible"]:
 		ensure  => "installed",
         }
 }
